308d833c7b3d7c424877aa2259c40d40be26bfbc
Harshaan Yadav <harshaanyadav.work@gmail.com>
2025-07-27 22:37:58 +0530

    Initial commit

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -2,19 +2,61 @@
 //  ViewController.swift
 //  ByteCoin
 //
-//  Created by Angela Yu on 11/09/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+// 1- Protocol added UIPickerViewDataSource
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        // to determine how many columns we want in our picker.
+        return 1
+    }
+    var coinManager = CoinManager()
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    
+    var fetchTimer: Timer?
 
-class ViewController: UIViewController {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        fetchTimer?.invalidate() // Cancel previous timer
+        let selectedCurrency = coinManager.currencyArray[row]
 
+        // Delay the API call slightly to avoid spamming on fast scrolls
+        fetchTimer = Timer.scheduledTimer(withTimeInterval: 0.3, repeats: false) { _ in
+            self.coinManager.getCoinPrice(for: selectedCurrency)
+        }
+    }
+    
+    func didUpdatePrice(price: String, currency: String) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = price
+            self.currencyLabel.text = currency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print(" Error: \(error.localizedDescription)")
+    }
+    
+    
+    
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        coinManager.delegate = self
     }
-
-
+    
+    
 }
 
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -7,13 +7,51 @@
 //
 
 import Foundation
+protocol CoinManagerDelegate {
+    func didUpdatePrice(price: String, currency: String)
+    func didFailWithError(error: Error)
+}
+
+import Foundation
 
 struct CoinManager {
     
-    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
-    
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RUB","SEK","SGD","USD","ZAR"]
 
-    
+    var delegate: CoinManagerDelegate?
+
+    func getCoinPrice(for currency: String) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=\(currency.lowercased())"
+
+        if let url = URL(string: urlString) {
+            let task = URLSession.shared.dataTask(with: url) { data, response, error in
+
+                if let error = error {
+                    self.delegate?.didFailWithError(error: error)
+                    return
+                }
+
+                if let safeData = data {
+                    if let price = self.parseJSON(safeData, for: currency.lowercased()) {
+                        let priceString = String(format: "%.2f", price)
+                        self.delegate?.didUpdatePrice(price: priceString, currency: currency)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+
+    func parseJSON(_ data: Data, for currency: String) -> Double? {
+        do {
+            if let decoded = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+               let bitcoin = decoded["bitcoin"] as? [String: Any],
+               let price = bitcoin[currency] as? Double {
+                return price
+            }
+        } catch {
+            delegate?.didFailWithError(error: error)
+        }
+        return nil
+    }
 }
